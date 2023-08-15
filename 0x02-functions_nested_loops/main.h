9d04189b058dff9a50fff6b1cbe873654904dfa1
Bert-dev <adesinaanuoluwapo2020@gmail.com>
2023-08-15 04:38:40 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,20 @@
 #ifndef main_h
 #define main_h
 
-int_putchar(char);
-void print_alphabet(void):
-void print alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+/*
+ * File: main.h
+ * Auth: Anuoluwapo
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
