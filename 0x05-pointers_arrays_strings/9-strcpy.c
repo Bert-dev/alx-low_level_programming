9a19c610adefbff51d80a19d0dc310f13a8b4534
Bert-dev <adesinaanuoluwapo2020@gmail.com>
2023-08-21 14:09:46 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+int index = 0;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+while (src[index])
+{
+dest[index] = src[index];
+index++;
+}
 
 return (0);
 }
