d67a4c4977286b70326c7bda42539061b9d79919
Bert-dev <adesinaanuoluwapo2020@gmail.com>
2023-08-21 03:55:28 -0700

    swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * Desc - function that swaps the
+ * values (a, b) of two integers
+ * Return:Always 0
+ */
+void swap_int(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
