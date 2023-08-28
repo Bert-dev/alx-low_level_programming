aea20d964f1b0c3cb0518060416c9f6b6c07857b
Bert-dev <adesinaanuoluwapo2020@gmail.com>
2023-08-28 06:31:08 -0700

    memcpyy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - Copies memeory area.
+ *@dest: source.
+ *@src: for filling.
+ *@n:lenght.
+ *Return: new.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i  = 0;
+while (i < n)
+{
+*(dest + 1) = *(src + 1);
+i++;
+}
+return (dest);
+}
