f70f51a5e361c4eb8f220859fc5851f7bb5a3e14
Bert-dev <adesinaanuoluwapo2020@gmail.com>
2023-09-07 05:51:19 -0700

    stressful but i did it

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of new memory array.
+ * Return: the string initialized.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr= malloc(b);
+if (ptr == NULL)
+{
+perror("Memory allocation failed\n");
+exit(98);
+}
+return (ptr);
+}
