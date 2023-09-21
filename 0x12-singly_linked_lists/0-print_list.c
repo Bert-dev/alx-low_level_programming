c8cfc05cfe3296ba7f5d69f0c89d91c20a87fc96
Bert-Dev <adesinaelizabeth7@gmail.com>
2023-09-21 10:29:57 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 /**
- ** print_listint - prints all the elements of a listint_t list.
+ * print_listint - prints all the elements of a listint_t list.
  * @h: pointer to the list header address
  * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
