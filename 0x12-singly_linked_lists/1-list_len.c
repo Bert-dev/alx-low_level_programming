8a07d8d52cda3cb3ff83beb6b4d22612fad0687d
Bert-Dev <adesinaelizabeth7@gmail.com>
2023-09-21 10:50:56 -0700

    lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len - shows the number of node of a listint_t list.
+ * @h: pointer to the header address
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
