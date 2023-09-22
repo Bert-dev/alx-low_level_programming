df90e743cc3a622573ca54921dfcedf74496ba76
Bert-Dev <adesinaelizabeth7@gmail.com>
2023-09-22 01:46:48 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("A tortoise, with a keen understanding of a hare's nature,\n");
+printf("challenges one to a race.\n");
+return (0);
+}
