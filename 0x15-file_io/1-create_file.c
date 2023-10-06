e55c796ebdebb73b3a730a3842e179f84b45c8d7
Bert-Dev <adesinaelizabeth7@gmail.com>
2023-10-06 01:14:43 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,32 +2,37 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * create_file - creates a file
- * @filename: name of file to be readed
+ * @filename: name of file to be read
  * @text_content: the text inside the file.
  * Return: 1 success, otherwise 0
  */
 
 int create_file(const char *filename, char *text_content)
 {
-int file_descriptor, w, len = 0;
+int file, wrote;
 if (filename == NULL)
-{
 return (-1);
-}
+file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (file == -1)
+return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len];)
-len++;
-}
-file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-w = write(file_descriptor, text_content, len);
-if (file_descriptor == -1 || w == -1)
+wrote = write(file, text_content, strlen(text_content));
+if (wrote == -1)
 {
+close(file);
 return (-1);
 }
-close(file_descriptor);
-return (-1);
+close(file);
+return (1);
+}
+else
+{
+close(file);
+return (1);
+}
 }
