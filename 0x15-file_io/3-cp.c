135b021ac1756862bc8efcead1248cd41e7b1211
Bert-Dev <adesinaelizabeth7@gmail.com>
2023-10-06 06:26:48 -0700

    cpc

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
@@ -6,48 +5,69 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
  * main- copy 1 file to another
  * @argc: argument counter
- * @argv: argument vector.
- * Return: 2 seccess, otherwise 0
+ * @argv: argument passed
+ * Return: 1 on success, otherwise 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int file_from, file_to;
-unsigned long int size;
-char *buffer;
+int src, dest, n_read = 1024, wrote, close_src, close_dest;
+unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+char buffer[1024];
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "%s", "usage: cp file_from file_to\n");
 exit(97);
 }
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
+src = open(argv[1], O_RDONLY);
+check_IO_stat(src, -1, argv[1], 'O');
+dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+check_IO_stat(dest, -1, argv[2], 'W');
+while (n_read == 1024)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[0]);
+n_read = read(src, buffer, sizeof(buffer));
+if (n_read == -1)
+check_IO_stat(-1, -1, argv[1], 'O');
+wrote = write(dest, buffer, n_read);
+if (wrote == -1)
+check_IO_stat(-1, -1, argv[2], 'W');
+}
+close_src = close(src);
+check_IO_stat(close_src, src, NULL, 'C');
+close_dest = close(dest);
+check_IO_stat(close_dest, dest, NULL, 'C');
+return (0);
+}
+
+/**
+ * check_IO_stat - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+
+void check_IO_stat(int stat, int fd, char *filename, char mode)
+{
+if (mode == 'C' && stat == 1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+else if (mode == 'O' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 exit(98);
 }
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
-if (file_to == -1)
+else if (mode == 'W' && stat == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 exit(99);
 }
-buffer = malloc(1024);
-while ((size = read(file_from, buffer, sizeof(buffer))) != 0)
-write(file_to, buffer, size);
-if (close(file_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error Can't close fd %d\n", file_from);
-exit(99);
-}
-if (close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(99);
-}
-free(buffer);
-return (0);
 }
